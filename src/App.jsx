89224dad9027d1feb5471eb274ef59b602dcfcdf
pnamistoso <phoebenicolea@gmail.com>
2025-03-12 21:51:39 +0800

    BrowserRouter set up
    dont forget to do npm i

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Navbar from "./Navbar.jsx";
+import Header from "./Header.jsx";
 import FaQ from "./FaQ.jsx";
 
 function App() {
   return (
-    <>
-      <FaQ />
-    </>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="faq" element={<FaQ />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
