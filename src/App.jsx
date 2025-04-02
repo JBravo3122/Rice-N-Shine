8ec15dbeec2dd892b8aaba639cccac84aa6d1ca4
JBravo3122 <jancarlosinoferio@gmail.com>
2025-04-02 23:39:14 +0800

    Added AboutUs Route

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import Header from "./Header.jsx";
 import FaQ from "./FaQ.jsx";
 import RecipeList from "./RecipeList.jsx";
 import Homepage from "./Homepage.jsx";
-import Video from "./Homepagevideo.jsx"
+import Video from "./Homepagevideo.jsx";
+import About from "./AboutUs.jsx";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/faq" element={<FaQ />} />
         <Route path="/recipelist" element={<RecipeList />} />
         <Route path="/homepage-video" element={<Video />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </>
   );
