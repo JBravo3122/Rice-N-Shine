0771a286902b3afdb4e9fea1fbc81a96ec8919f2
JBravo3122 <jancarlosinoferio@gmail.com>
2025-04-02 19:50:22 +0800

    Added route watch video butt to video

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Header from "./Header.jsx";
 import FaQ from "./FaQ.jsx";
 import RecipeList from "./RecipeList.jsx";
 import Homepage from "./Homepage.jsx";
+import Video from "./Homepagevideo.jsx"
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="/faq" element={<FaQ />} />
         <Route path="/recipelist" element={<RecipeList />} />
+        <Route path="/homepage-video" element={<Video />} />
       </Routes>
     </>
   );
