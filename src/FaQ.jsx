a474028cd973f22777cdce820f34ad34a5ed7a26
JBravo3122 <jancarlosinoferio@gmail.com>
2025-04-29 14:20:11 +0800

    Responsive FaQ

--- src/FaQ.jsx
+++ src/FaQ.jsx
@@ -23,9 +23,34 @@ function FaQ() {
         "Most ingredients are readily available in supermarkets or local markets. When certain items are less common, we provide suitable alternatives.",
     },
     {
+      question: "Are the photos of the actual dishes?",
+      answer:
+        "The photos on our website are carefully selected from trusted online sources to closely match the dishes. We make sure they accurately represent what you can expect when following the recipes.",
+    },
+    {
+      question: "How can I search for a specific recipe?",
+      answer:
+        "You can use the search bar at the top of the page to find recipes by their name.",
+    },
+    {
+      question: "Are there recipes for beginners?",
+      answer:
+        "Yes! All of our recipes are designed to be easy to follow and suitable for beginners. We make sure the instructions are clear and simple, so anyone can cook with confidence.",
+    },    
+    {
+      question: "Do you feature recipes from different cuisines?",
+      answer:
+        "We focus on Filipino cuisine and proudly feature dishes from all over the Philippines, sorted by region to showcase the country’s rich and diverse culinary traditions.",
+    },
+    {
       question: "Can I share recipes on social media?",
       answer:
         "Absolutely! Each recipe page includes social sharing buttons, making it easy for you to share your favorite dishes with friends and family.",
+    },
+    {
+      question: "Do I need an account to use the website?",
+      answer:
+        "No account is needed to browse and use the recipes! All our recipes and features are completely free and accessible to everyone."
     },
   ];
 
