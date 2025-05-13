b8ddd9ee96aa2c9651c97b16cd8102b75e2ae153
pnamistoso <phoebenicolea@gmail.com>
2025-05-13 23:24:51 +0800

    search and regions

--- src/RecipeList.jsx
+++ src/RecipeList.jsx
@@ -6,13 +6,34 @@ import { supabase } from "./supabaseclient.js";
 
 function RecipeList() {
   const [recipes, setRecipes] = useState([]); // where we store the recipes
+  const [searchQuery, setSearchQuery] = useState("");
   const [selectedGroup, setSelectedGroup] = useState(null);
-  const [selectedRegion, setSelectedRegion] = useState(null);
+  const [selectedRegion, setSelectedRegion] = useState({});
   const groupToRegionIds = {
     1: [1, 2, 3, 4, 5, 6, 7, 8],
     2: [9, 10, 11],
     3: [12, 13, 14, 15, 16, 17, 18],
   };
+
+  const regionNames = [
+    "Region I",
+    "Region II",
+    "Region III",
+    "Region IV-A",
+    "Region IV-B",
+    "Region V",
+    "Region VI",
+    "Region VII",
+    "Region VIII",
+    "Region IX",
+    "Region X",
+    "Region XI",
+    "Region XIII",
+    "NCR",
+    "CAR",
+    "BARMM",
+    "NIR",
+  ];
 
   useEffect(() => {
     const fetchRecipes = async () => {
@@ -28,8 +49,12 @@ function RecipeList() {
         )
         .order("region_id", { ascending: true }); // fetches all recipes
 
-      if (selectedRegion) {
-        query = query.eq("region_id", selectedRegion);
+      const selectedRegionNames = Object.keys(selectedRegion).filter(
+        (region) => selectedRegion[region]
+      );
+
+      if (selectedRegionNames.length > 0) {
+        query = query.in("regions.region_name", selectedRegionNames);
       } else if (selectedGroup !== null) {
         const regionIds = groupToRegionIds[selectedGroup];
         if (regionIds) {
@@ -49,28 +74,37 @@ function RecipeList() {
             : "/default-image.jpg",
           region_name: recipe.regions?.region_name || "Unknown Region",
         }));
-        setRecipes(uniqueRecipes);
+
+        const filteredData = uniqueRecipes.filter((recipe) =>
+          recipe.recipe_name.toLowerCase().includes(searchQuery.toLowerCase())
+        );
+
+        setRecipes(filteredData);
       }
     };
 
     fetchRecipes();
-  }, [selectedGroup, selectedRegion]);
+  }, [selectedGroup, selectedRegion, searchQuery]);
 
   return (
     <>
-    
       <div className="reclist-container">
         <div className="breadcrumbs">
           <p>Home &gt; Recipes</p>
         </div>
-        <SearchBar />
+        <SearchBar
+          searchQuery={searchQuery}
+          setSearchQuery={setSearchQuery}
+          selectedRegions={selectedRegion}
+          setSelectedRegions={setSelectedRegion}
+        />
         <div className="reclist-body">
           <div className="reclist-tabButtons">
             <div className="reclist-allButtons">
               <button
                 type="button"
                 onClick={() => {
-                  setSelectedRegion(null);
+                  setSelectedRegion({});
                   setSelectedGroup(null);
                 }}
               >
@@ -79,7 +113,7 @@ function RecipeList() {
               <button
                 type="button"
                 onClick={() => {
-                  setSelectedRegion(null);
+                  setSelectedRegion({});
                   setSelectedGroup(1);
                 }}
               >
@@ -88,7 +122,7 @@ function RecipeList() {
               <button
                 type="button"
                 onClick={() => {
-                  setSelectedRegion(null);
+                  setSelectedRegion({});
                   setSelectedGroup(2);
                 }}
               >
@@ -97,7 +131,7 @@ function RecipeList() {
               <button
                 type="button"
                 onClick={() => {
-                  setSelectedRegion(null);
+                  setSelectedRegion({});
                   setSelectedGroup(3);
                 }}
               >
@@ -106,211 +140,6 @@ function RecipeList() {
             </div>
           </div>
           <div className="allRecipes-container">
-            {/* <div className="reclist-sideBar">
-              <h4>Regions</h4>
-              <div className="reclist-regionlist">
-                <ul>
-                  <li>
-                    <a
-                      href="#"
-                      onClick={() => {
-                        setSelectedRegion(1);
-                        setSelectedGroup("All");
-                      }}
-                    >
-                      Region I
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      onClick={() => {
-                        setSelectedRegion(2);
-                        setSelectedGroup("All");
-                      }}
-                    >
-                      Region II
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      onClick={() => {
-                        setSelectedRegion(3);
-                        setSelectedGroup("All");
-                      }}
-                    >
-                      Region III
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      onClick={() => {
-                        setSelectedRegion(4);
-                        setSelectedGroup("All");
-                      }}
-                    >
-                      Region IV-A
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      onClick={() => {
-                        setSelectedRegion(5);
-                        setSelectedGroup("All");
-                      }}
-                    >
-                      Region IV-B
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      onClick={() => {
-                        setSelectedRegion(6);
-                        setSelectedGroup("All");
-                      }}
-                    >
-                      Region V
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      onClick={() => {
-                        setSelectedRegion(7);
-                        setSelectedGroup("All");
-                      }}
-                    >
-                      NCR
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      onClick={() => {
-                        setSelectedRegion(8);
-                        setSelectedGroup("All");
-                      }}
-                    >
-                      CAR
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      onClick={() => {
-                        setSelectedRegion(9);
-                        setSelectedGroup("All");
-                      }}
-                    >
-                      Region VI
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      onClick={() => {
-                        setSelectedRegion(10);
-                        setSelectedGroup("All");
-                      }}
-                    >
-                      Region VII
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      onClick={() => {
-                        setSelectedRegion(11);
-                        setSelectedGroup("All");
-                      }}
-                    >
-                      NIR
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      onClick={() => {
-                        setSelectedRegion(12);
-                        setSelectedGroup("All");
-                      }}
-                    >
-                      Region VII
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      onClick={() => {
-                        setSelectedRegion(13);
-                        setSelectedGroup("All");
-                      }}
-                    >
-                      Region IX
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      onClick={() => {
-                        setSelectedRegion(14);
-                        setSelectedGroup("All");
-                      }}
-                    >
-                      Region X
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      onClick={() => {
-                        setSelectedRegion(15);
-                        setSelectedGroup("All");
-                      }}
-                    >
-                      Region XI
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      onClick={() => {
-                        setSelectedRegion(16);
-                        setSelectedGroup("All");
-                      }}
-                    >
-                      Region XII
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      onClick={() => {
-                        setSelectedRegion(17);
-                        setSelectedGroup("All");
-                      }}
-                    >
-                      Region XIII
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      onClick={() => {
-                        setSelectedRegion(18);
-                        setSelectedGroup("All");
-                      }}
-                    >
-                      BARMM
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div> */}
             <div className="reclist-recipes">
               {recipes.length > 0
                 ? recipes.map((recipe) => (
--- src/SearchBar.jsx
+++ src/SearchBar.jsx
@@ -1,21 +1,14 @@
 import React, { useState, useEffect, useRef } from "react";
 import "./css/SearchBar.css";
-import { Link } from "react-router-dom";
-import { supabase } from "./supabaseclient.js";
 
-function SearchBar() {
+function SearchBar({
+  searchQuery,
+  setSearchQuery,
+  selectedRegions,
+  setSelectedRegions,
+}) {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-  const [searchQuery, setSearchQuery] = useState("");
   const inputRef = useRef(null);
-
-  const [selectedRegions, setSelectedRegions] = useState({
-    "Region I": false, "Region II": false, "Region III": false,
-    "Region IV-A": false, "Region IV-B": false, "Region V": false,
-    "Region VI": false, "Region VII": false, "Region VIII": false,
-    "Region IX": false, "Region X": false, "Region XI": false,
-    "Region XIII": false, "NCR": false, "CAR": false,
-    "BARMM": false, "NIR": false,
-  });
 
   // Detect if browser supports Speech Recognition
   const isSpeechSupported =
@@ -74,7 +67,10 @@ function SearchBar() {
 
   useEffect(() => {
     const handleClickOutside = (e) => {
-      if (!e.target.closest("#filterDropdown") && !e.target.closest(".filter-wrapper")) {
+      if (
+        !e.target.closest("#filterDropdown") &&
+        !e.target.closest(".filter-wrapper")
+      ) {
         closeDropdown();
       }
     };
@@ -87,7 +83,9 @@ function SearchBar() {
   return (
     <div className="searchbarcontainer">
       <h1>Regions N' Recipes</h1>
-      <p><i>Search and Taste the Philippines, One Region at a Time</i></p>
+      <p>
+        <i>Search and Taste the Philippines, One Region at a Time</i>
+      </p>
 
       <div className="searchbar">
         <input
@@ -100,78 +98,194 @@ function SearchBar() {
         <button>Search</button>
 
         {isSpeechSupported ? (
-            <button onClick={startListening} aria-label="Voice search" className="voice-btn">
-                Voice Search
-            </button>
+          <button
+            onClick={startListening}
+            aria-label="Voice search"
+            className="voice-btn"
+          >
+            Voice Search
+          </button>
         ) : (
-            <button disabled title="Voice input is not supported in this browser" className="voice-btn">
-                Voice Search
-            </button>
+          <button
+            disabled
+            title="Voice input is not supported in this browser"
+            className="voice-btn"
+          >
+            Voice Search
+          </button>
         )}
-
 
         <div className="filter-wrapper" onClick={toggleDropdown}>
           <button type="button">Filters</button>
           {isDropdownOpen && (
-            <div id="filterDropdown" className="dropdown-content" onClick={stopDropdownClose}>
+            <div
+              id="filterDropdown"
+              className="dropdown-content"
+              onClick={stopDropdownClose}
+            >
               <div className="clearContainer">
                 <label id="filterTitle">Filter by Region(s)</label>
-                <button type="button" onClick={clearAllSelections}>Clear All</button><br />
+                <button type="button" onClick={clearAllSelections}>
+                  Clear All
+                </button>
+                <br />
               </div>
 
               <div className="filterdropdown">
                 <label className="filter-item">
-                       <input type="checkbox" checked={selectedRegions["Region I"]} onChange={() => toggleCheckbox("Region I") } /> Region I
-                            </label><br />
-                            <label className="filter-item">
-                            <input type="checkbox" checked={selectedRegions["Region II"]} onChange={() => toggleCheckbox("Region II")} /> Region II
-                            </label><br />
-                                    <label className="filter-item">
-                                        <input type="checkbox" checked={selectedRegions["Region III"]} onChange={() => toggleCheckbox("Region III")} /> Region III
-                                    </label><br />
-                                    <label className="filter-item">
-                                        <input type="checkbox" checked={selectedRegions["Region IV-A"]} onChange={() => toggleCheckbox("Region IV-A")} /> Region IV-A
-                                    </label><br />
-                                    <label className="filter-item">
-                                        <input type="checkbox" checked={selectedRegions["Region IV-B"]} onChange={() => toggleCheckbox("Region IV-B")} /> Region IV-B
-                                    </label><br />
-                                    <label className="filter-item">
-                                        <input type="checkbox" checked={selectedRegions["Region V"]} onChange={() => toggleCheckbox("Region V")} /> Region V
-                                    </label><br />
-                                    <label className="filter-item">
-                                        <input type="checkbox" checked={selectedRegions["Region VI"]} onChange={() => toggleCheckbox("Region VI")} /> Region VI
-                                    </label><br />
-                                    <label className="filter-item">
-                                        <input type="checkbox" checked={selectedRegions["Region VII"]} onChange={() => toggleCheckbox("Region VII")} /> Region VII
-                                    </label><br />
-                                    <label className="filter-item">
-                                        <input type="checkbox" checked={selectedRegions["Region VIII"]} onChange={() => toggleCheckbox("Region VIII")} /> Region VIII
-                                    </label><br />
-                                    <label className="filter-item">
-                                        <input type="checkbox" checked={selectedRegions["Region IX"]} onChange={() => toggleCheckbox("Region IX")} /> Region IX
-                                    </label><br />
-                                    <label className="filter-item">
-                                        <input type="checkbox" checked={selectedRegions["Region X"]} onChange={() => toggleCheckbox("Region X")} /> Region X
-                                    </label><br />
-                                    <label className="filter-item">
-                                        <input type="checkbox" checked={selectedRegions["Region XI"]} onChange={() => toggleCheckbox("Region XI")} /> Region XI
-                                    </label><br />
-                                    <label className="filter-item">
-                                        <input type="checkbox" checked={selectedRegions["Region XIII"]} onChange={() => toggleCheckbox("Region XIII")} /> Region XIII
-                                    </label><br />
-                                    <label className="filter-item">
-                                        <input type="checkbox" checked={selectedRegions["NCR"]} onChange={() => toggleCheckbox("NCR")} /> NCR
-                                    </label><br />
-                                    <label className="filter-item">
-                                        <input type="checkbox" checked={selectedRegions["CAR"]} onChange={() => toggleCheckbox("CAR")} /> CAR
-                                    </label><br />
-                                    <label className="filter-item">
-                                        <input type="checkbox" checked={selectedRegions["BARMM"]} onChange={() => toggleCheckbox("BARMM")} /> BARMM
-                                    </label><br />
-                                    <label className="filter-item">
-                                        <input type="checkbox" checked={selectedRegions["NIR"]} onChange={() => toggleCheckbox("NIR")} /> NIR
-                                    </label><br />
-                                </div>                 
+                  <input
+                    type="checkbox"
+                    checked={selectedRegions["Region I"]}
+                    onChange={() => toggleCheckbox("Region I")}
+                  />{" "}
+                  Region I
+                </label>
+                <br />
+                <label className="filter-item">
+                  <input
+                    type="checkbox"
+                    checked={selectedRegions["Region II"]}
+                    onChange={() => toggleCheckbox("Region II")}
+                  />{" "}
+                  Region II
+                </label>
+                <br />
+                <label className="filter-item">
+                  <input
+                    type="checkbox"
+                    checked={selectedRegions["Region III"]}
+                    onChange={() => toggleCheckbox("Region III")}
+                  />{" "}
+                  Region III
+                </label>
+                <br />
+                <label className="filter-item">
+                  <input
+                    type="checkbox"
+                    checked={selectedRegions["Region IV-A"]}
+                    onChange={() => toggleCheckbox("Region IV-A")}
+                  />{" "}
+                  Region IV-A
+                </label>
+                <br />
+                <label className="filter-item">
+                  <input
+                    type="checkbox"
+                    checked={selectedRegions["Region IV-B"]}
+                    onChange={() => toggleCheckbox("Region IV-B")}
+                  />{" "}
+                  Region IV-B
+                </label>
+                <br />
+                <label className="filter-item">
+                  <input
+                    type="checkbox"
+                    checked={selectedRegions["Region V"]}
+                    onChange={() => toggleCheckbox("Region V")}
+                  />{" "}
+                  Region V
+                </label>
+                <br />
+                <label className="filter-item">
+                  <input
+                    type="checkbox"
+                    checked={selectedRegions["Region VI"]}
+                    onChange={() => toggleCheckbox("Region VI")}
+                  />{" "}
+                  Region VI
+                </label>
+                <br />
+                <label className="filter-item">
+                  <input
+                    type="checkbox"
+                    checked={selectedRegions["Region VII"]}
+                    onChange={() => toggleCheckbox("Region VII")}
+                  />{" "}
+                  Region VII
+                </label>
+                <br />
+                <label className="filter-item">
+                  <input
+                    type="checkbox"
+                    checked={selectedRegions["Region VIII"]}
+                    onChange={() => toggleCheckbox("Region VIII")}
+                  />{" "}
+                  Region VIII
+                </label>
+                <br />
+                <label className="filter-item">
+                  <input
+                    type="checkbox"
+                    checked={selectedRegions["Region IX"]}
+                    onChange={() => toggleCheckbox("Region IX")}
+                  />{" "}
+                  Region IX
+                </label>
+                <br />
+                <label className="filter-item">
+                  <input
+                    type="checkbox"
+                    checked={selectedRegions["Region X"]}
+                    onChange={() => toggleCheckbox("Region X")}
+                  />{" "}
+                  Region X
+                </label>
+                <br />
+                <label className="filter-item">
+                  <input
+                    type="checkbox"
+                    checked={selectedRegions["Region XI"]}
+                    onChange={() => toggleCheckbox("Region XI")}
+                  />{" "}
+                  Region XI
+                </label>
+                <br />
+                <label className="filter-item">
+                  <input
+                    type="checkbox"
+                    checked={selectedRegions["Region XIII"]}
+                    onChange={() => toggleCheckbox("Region XIII")}
+                  />{" "}
+                  Region XIII
+                </label>
+                <br />
+                <label className="filter-item">
+                  <input
+                    type="checkbox"
+                    checked={selectedRegions["NCR"]}
+                    onChange={() => toggleCheckbox("NCR")}
+                  />{" "}
+                  NCR
+                </label>
+                <br />
+                <label className="filter-item">
+                  <input
+                    type="checkbox"
+                    checked={selectedRegions["CAR"]}
+                    onChange={() => toggleCheckbox("CAR")}
+                  />{" "}
+                  CAR
+                </label>
+                <br />
+                <label className="filter-item">
+                  <input
+                    type="checkbox"
+                    checked={selectedRegions["BARMM"]}
+                    onChange={() => toggleCheckbox("BARMM")}
+                  />{" "}
+                  BARMM
+                </label>
+                <br />
+                <label className="filter-item">
+                  <input
+                    type="checkbox"
+                    checked={selectedRegions["NIR"]}
+                    onChange={() => toggleCheckbox("NIR")}
+                  />{" "}
+                  NIR
+                </label>
+                <br />
+              </div>
             </div>
           )}
         </div>
@@ -179,7 +293,10 @@ function SearchBar() {
 
       {!isSpeechSupported && (
         <p className="speech-warning">
-          <i style={{color: "gray"}}>Your browser does not support voice input. Try another browser for the best experience.</i>
+          <i style={{ color: "gray" }}>
+            Your browser does not support voice input. Try another browser for
+            the best experience.
+          </i>
         </p>
       )}
     </div>
